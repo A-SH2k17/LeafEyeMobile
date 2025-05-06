01bca6040a673b6cc299f1dcfa8336cced444cc8
marflame <maro.tamer.2003@gmail.com>
2025-05-07 00:30:39 +0300

    Fertilizer reccomendation with mock values

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <Stack.Screen name="(fertilizerreccomendation)" options={{ headerShown: false}} />
         <Stack.Screen name="(logreg)" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
